38643a482551bbcca2562badc73ee20fd0ae6be8
rafael1209 <mr.chasmanrafael@gmail.com>
2024-08-09 12:05:19 +0300

    updated git action

---
+++ Controllers/AuthorizeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UnityWebCore.Controllers
+{
+        [Route("api/auth")]
+        [ApiController]
+        public class AuthorizeController : Controller
+        {
+                public AuthorizeController()
+                {
+
+                }
+
+                [HttpPost("register")]
+                public IActionResult Register()
+                {
+                        return Ok();
+                }
+        }
+}
